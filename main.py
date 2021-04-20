d1d001e281b2989b56836019b2f177ff0dda0133
Yufeng <337916139@qq.com>
2021-04-19 21:38:55 -0400

    add keyword logic & output file

--- main.py
+++ main.py
@@ -1,12 +1,52 @@
 import certstream
+import os
+import time
+import yaml
 
-certstream_calidog_url = 'wss://certstream.calidog.io'
+root = os.path.dirname(os.path.realpath(__file__))
+
+log_output = root + '/phishing_domains_' + time.strftime("%Y-%m-%d") + '.log'
+
+keyword_yaml = root + '/keyword.yaml'
+keyword = None
 
 
-def listen_func(message, context):
-    pass
-    # todo
+def get_phishing_score(domain):
+    score = 0
+    sub_domains = domain.split('.')
+
+    if len(sub_domains) < 2:
+        return
+
+    top_level_domain = sub_domains[-1]
+    second_level_domain = sub_domains[-2]
+
+    # TODO: avoid good domains (e.g. google.com)
+    if second_level_domain in keyword_yaml['whitelist']:
+        return
+
+    # TODO: score by top_level_domain in the keyword.yaml
+    if top_level_domain in keyword_yaml['tlds']:
+        score += 10
+
+    return score
+
+
+def listen_func(message, _):
+    if message['message_type'] != "certificate_update":
+        return
+
+    all_domains = message['data']['leaf_cert']['all_domains']
+    for domain in all_domains:
+        print(domain)
+        phishing_score = get_phishing_score(domain.lower())
+
+        if phishing_score >= 75:
+            with open(log_output, 'a') as f:
+                f.write("{}\n".format(domain))
 
 
 if __name__ == '__main__':
-    certstream.listen_for_events(listen_func, url=certstream_calidog_url)
+    with open(keyword_yaml, 'r') as f:
+        keyword = yaml.safe_load(f)
+    certstream.listen_for_events(listen_func, url='wss://certstream.calidog.io')

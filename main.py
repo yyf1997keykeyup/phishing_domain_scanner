b7ddf222fb08d28f319bfb0d780c90753f42f5e2
Yufeng <337916139@qq.com>
2021-04-27 19:25:14 -0400

    daily update 2.0

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import certstream
 import os
 import time
 import yaml
+from Levenshtein import distance
 
 root = os.path.dirname(os.path.realpath(__file__))
 
@@ -13,6 +14,10 @@ keyword = None
 
 def get_phishing_score(domain):
     score = 0
+
+    if domain.startswith('*.'):
+        domain = domain[2:]
+
     sub_domains = domain.split('.')
 
     if len(sub_domains) < 2:
@@ -22,7 +27,7 @@ def get_phishing_score(domain):
     second_level_domain = sub_domains[-2]
 
     # TODO: avoid good domains (e.g. google.com)
-    if second_level_domain in keyword['whitelist']:
+    if top_level_domain == 'com' and second_level_domain in keyword['whitelist']:
         return score
 
     for sub_domain in sub_domains:
@@ -32,6 +37,21 @@ def get_phishing_score(domain):
     # TODO: score by top_level_domain in the keyword.yaml
     if top_level_domain in keyword['tlds']:
         score += 10
+
+    # Lots of '-'
+    if domain.count('-') >= 4:
+        score += domain.count('-') * 5
+
+    # Lots of '.'
+    if domain.count('.') >= 4:
+        score += domain.count('.') * 3
+
+    # Levenshtein distance (ie. paypol)
+    keywords_list = keyword['generic'] = keyword['whitelist']
+    for key, key_score in keywords_list.items():
+        for domain_word in sub_domains:
+            if distance(str(domain_word), str(key)) == 1:
+                score += key_score * 2
 
     return score
 
@@ -45,9 +65,11 @@ def listen_func(message, _):
         phishing_score = get_phishing_score(domain.lower())
 
         if phishing_score >= 75:
-            print(domain)
+            print('suspicious: ' + domain + '  ' + str(phishing_score))
             with open(log_output, 'a') as f:
                 f.write("{}\n".format(domain))
+        # elif phishing_score != 0:
+        #     print('pass: ' + domain + '  ' + str(phishing_score))
 
 
 if __name__ == '__main__':
